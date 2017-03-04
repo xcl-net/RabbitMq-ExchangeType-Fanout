43717500441906e9019faa277dc0464391f2e7eb
肖传令 <peter.xiao.beijng@gmail.com>
2017-03-04 23:38:37 +0800

    交换机类型为fanout，绑定的队列接受的消息是相同的

---
+++ 消息生产者publish/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace 消息生产者publish
+{
+    /// <summary>
+    /// 消息生产，并把消息发送到交换机中
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "127.0.0.1";
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            for (int i = 0; i < 100; i++)
+            {
+                var msg = Encoding.UTF8.GetBytes($"这是第{i}个消息体");
+                channel.BasicPublish("myFanoutExchange",routingKey:string.Empty,basicProperties:null,body:msg);
+                Console.WriteLine($"第{i}个消息已推送到交换机");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ 队列queue2绑定到交换机并创建消费者consumer2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace 队列queue2绑定到交换机并创建消费者consumer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory();
+            factory.HostName = "127.0.0.1";
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            //声明一个交换机
+            channel.ExchangeDeclare("myFanoutExchange", ExchangeType.Fanout, true, false, null);
+            Console.WriteLine("交换机创建完成：myFanoutExchange ...");
+            channel.QueueDeclare("myFanoutQueue2", true, false, false, null);
+            Console.WriteLine("队列创建完成：myFanoutQueue2 ...");
+            channel.QueueBind("myFanoutQueue2", "myFanoutExchange", string.Empty, null);
+            var consumer = new EventingBasicConsumer(channel);
+            Console.WriteLine("队列：myFanoutQueue2的消费者，创建完成 ...");
+            consumer.Received += (sender, e) =>
+            {
+                var msg = Encoding.UTF8.GetString(e.Body);
+                Console.WriteLine(msg);
+            };
+            channel.BasicConsume("myFanoutQueue2", true, consumer);
+            Console.WriteLine("consumer 启动启动完成。");
+            Console.Read();
+        }
+    }
+}
